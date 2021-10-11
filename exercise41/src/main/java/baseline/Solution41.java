778a941d5be2246e55f3c94478ee1652296120bf
WhorTez <dortiz1@knights.ucf.edu>
2021-10-11 11:23:02 -0400

    Pseudocode for exercise41

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,25 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 4 Solutions
+ * Copyright 2021 Duane Ortiz
+ */
+package baseline;
+
+public class Solution41 {
+    /*
+    Create a reader object
+    Create arrayList in order to store strings
+    Create variable to assign each string
+
+    Use a loop to read the input file
+    from the file read in each name and add it to the arrayList
+    repeat for each line
+
+    Use sort method to sort all the strings
+    Create new object to write a file
+
+    Use a loop to re-write the input file into the output file
+
+    Print out the output file with the number of names in the array
+    Make sure they are printed out in alphabetical order of Last names
+     */
+}
