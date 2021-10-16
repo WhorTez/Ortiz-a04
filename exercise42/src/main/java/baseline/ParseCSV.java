896c49743edc07b79c16af9f7cef6d95a93df5e3
WhorTez <dortiz1@knights.ucf.edu>
2021-10-16 00:56:16 -0400

    pseudocode for exercise42

---
+++ exercise42/src/main/java/baseline/ParseCSV.java
@@ -0,0 +1,29 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParseCSV {
+
+    public List<String>  seperateData() {
+       ArrayList<String> seperateData = new ArrayList<>();
+       //loop size of list
+            //for each comma split the string
+            //also store it to a local variable to add it to seperateData
+    }
+    public List<String> lastNameArray(){
+        //loop length of array
+            //get the first element and skip two each line
+            //add last names to lastNameArrayList
+    }
+    public List<String> firstNameArray(){
+        //loop length of array
+        //get the second element and skip two each line
+        //add last names to firstNameArrayList
+    }
+    public List<String> salaryArray(){
+        //loop length of array
+        //get the third element and skip two each line
+        //add last names to salaryArrayList
+    }
+}
---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,29 @@
+package baseline;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution42 {
+
+    private File data = new File("data/exercise42_input.txt");
+
+    public List<String> getFileData(){
+        //create a buffered reader insdie
+        //return the created string
+    }
+
+    public void outputFileData(){
+        //create a buffered writer
+        //for loop through length of the list
+        // print out last name, first name and salary
+    }
+
+    public static void main(String[] args){
+        ParseCSV csv = new ParseCSV();
+
+
+    }
+
+}
