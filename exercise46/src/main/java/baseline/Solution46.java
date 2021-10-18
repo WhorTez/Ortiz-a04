6296b9208536c8b340217ecb8dbd6c4f6dc8bb59
WhorTez <dortiz1@knights.ucf.edu>
2021-10-17 23:25:23 -0400

    updatde pseudocode

--- exercise46/src/main/java/baseline/Solution46.java
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -4,10 +4,31 @@
  */
 package baseline;
 
+import java.util.List;
+import java.util.Map;
+
+
 public class Solution46 {
-    public static void main(String[] args){
-        //count frequency of a repeated word in the input file
-        //sort wordFrequency array by frequency
-        //Generate and print formatted output
+    public String getFileData(){
+        //create a buffered reader
+            //while next line is not null
+        //return created string
     }
+
+    public Map<String, Integer> countFrequencyOfWord(){
+        //split String into an array
+        //loop through array
+            //add each word to array list
+        //sort array list
+        //loop through array lsit
+        //return new map
+    }
+
+    public void writeToFile(Map<String, Integer> wordCount){
+        //create buffered writer
+            //create buffered reader
+                //for loop for size of map
+                    //print out frequency of each word with *
+    }
+
 }
