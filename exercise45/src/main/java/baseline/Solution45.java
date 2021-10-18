87b15167cfae9a04b2303ec8733b752c4f51bfa7
WhorTez <dortiz1@knights.ucf.edu>
2021-10-17 20:24:04 -0400

    psuedocode for exercise45

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,44 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 4 Solutions
+ * Copyright 2021 Duane Ortiz
+ */
+package baseline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution45 {
+    public String outputName;
+
+    public List<String> getFileData(){
+        List<String> fileInput = new ArrayList<>();
+        //each element in the array holds a line of the input file
+        //create a buffered reader
+            //while next line is not null
+        // return the created array
+    }
+
+    public List<String> replaceUtilizeWithUse(){
+        List<String> replacedList = new ArrayList<>();
+
+        //loop through each element and find each occurence of utilize with used
+        //assign the replaced element
+
+        //return replacedList
+    }
+
+    public String getOutputFileName(){
+        //return the name designated by the user for the output file
+    }
+
+    public void writeToFile(String outputName){
+
+    }
+
+    public static void main(String[] args){
+        //get file data
+        //create new paragraph
+        //write the paragraph to new file
+    }
+
+}
