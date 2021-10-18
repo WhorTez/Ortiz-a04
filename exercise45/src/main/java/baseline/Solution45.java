e8c4767f49c3dee66b4f1f7f468e5d6bd591f942
WhorTez <dortiz1@knights.ucf.edu>
2021-10-17 21:04:37 -0400

    completed exercise45

--- exercise45/src/main/java/baseline/Solution45.java
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -6,39 +6,75 @@ package baseline;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 
 public class Solution45 {
-    public String outputName;
+    private static final Scanner in = new Scanner(System.in);
+    private static final File data = new File("data/exercise45_input.txt");
+    public List<String> fileInput = new ArrayList<>();
+    public List<String> replacedList = new ArrayList<>();
+
 
     public List<String> getFileData(){
-        List<String> fileInput = new ArrayList<>();
+
         //each element in the array holds a line of the input file
-        //create a buffered reader
-            //while next line is not null
+        try {
+            //create a buffered reader
+            try (Scanner scan = new Scanner(data)) {
+                //while next line is not null
+                while (scan.hasNextLine()) {
+                    fileInput.add(scan.nextLine());
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         // return the created array
+        return fileInput;
     }
 
     public List<String> replaceUtilizeWithUse(){
-        List<String> replacedList = new ArrayList<>();
 
         //loop through each element and find each occurence of utilize with used
-        //assign the replaced element
-
+        for(int i=0;i<fileInput.size(); i++) {
+            //assign the replaced element
+            String willReplace = fileInput.get(i).replace("utilizes", "uses");
+            willReplace = willReplace.replace("utilize", "use");
+            replacedList.add(willReplace);
+        }
         //return replacedList
+        return replacedList;
     }
 
     public String getOutputFileName(){
+        System.out.print("What do you want to name the output file? ");
         //return the name designated by the user for the output file
+        return in.next();
     }
 
     public void writeToFile(String outputName){
-
+        try{
+            try(BufferedWriter bw = new BufferedWriter(new FileWriter("data\\"+ outputName +".txt"))){
+                for(int i=0;i<fileInput.size();i++){
+                    bw.write(replacedList.get(i) +"\n");
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args){
+        Solution45 s = new Solution45();
         //get file data
+        s.fileInput = s.getFileData();
         //create new paragraph
+        s.replacedList = s.replaceUtilizeWithUse();
         //write the paragraph to new file
+        s.writeToFile(s.getOutputFileName());
     }
 
 }
--- exercise45/src/test/java/baseline/Solution45Test.java
+++ exercise45/src/test/java/baseline/Solution45Test.java
@@ -1,20 +1,50 @@
 package baseline;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
 
 class Solution45Test {
 
     @Test
     void getFileData() {
+        Solution45 s = new Solution45();
+        String test = "One should never utilize the word \"utilize\" in writing. Use \"use\" instead.";
+        s.fileInput = s.getFileData();
+        assertEquals(test, s.fileInput.get(0));
     }
 
     @Test
     void replaceUtilizeWithUse() {
+        Solution45 s = new Solution45();
+        String test = "One should never use the word \"use\" in writing. Use \"use\" instead.";
+        s.fileInput = s.getFileData();
+        s.replacedList = s.replaceUtilizeWithUse();
+        assertEquals(test, s.replacedList.get(0));
     }
 
     @Test
     void writeToFile() {
+        Solution45 s = new Solution45();
+        s.fileInput = s.getFileData();
+        s.replacedList = s.replaceUtilizeWithUse();
+        s.writeToFile("test");
+
+        List<String> test = new ArrayList<>();
+
+        File data = new File("data/test.txt");
+
+        try (Scanner scan = new Scanner(data)){
+            while (scan.hasNextLine()){
+                test.add(scan.nextLine());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        assertEquals(s.replacedList.get(0), test.get(0));
     }
 }
